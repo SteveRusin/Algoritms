d061cb5f5d4dad0222fa5bebceda04cc4cc7ee6b
Steve <steve.neeson21@gmail.com>
2018-05-28 22:41:04 +0300

    donemerge sort

--- main.js
+++ main.js
@@ -1,26 +1,39 @@
 const array = [1, 2, 5, 6, 8, 10, 15, 19, 25, 28, 29, 32, 35, 41, 46, 48, 50];
 const randomArr = shuffle(array);
 
-function shellSort(arr, searchValue) {
-    let gap = Math.floor(arr.length / 2);
-
-    for (let i = 0; i + gap < arr.length && gap >= 1; i++, gap = gap / 2) {
-        if (arr[i] > arr[i + gap]) {
-            
-            swap(arr, i, i + gap);
-
-            for (let j = i; arr[j] > arr[j + gap] && gap === 1; j--) {
-                swap(arr, j, j + gap)
-            }
-
-        }
+function mergeSort(arr) {
+    if (arr.length === 1) {
+        return arr;
     }
 
-    return arr;
+    const middle = Math.floor(arr.length / 2);
+    const leftPart = arr.slice(0, middle);
+    const rightPart = arr.slice(middle);
+
+    return merge(mergeSort(leftPart), mergeSort(rightPart))
 }
 
+function merge(left, right) {
+    const result = [];
+    let poinerLeft = 0;
+    let pointerRight = 0;
 
-console.log(shellSort(array, 48));
+    while (poinerLeft < left.length && pointerRight < right.length) {
+        if (left[poinerLeft] < right[pointerRight]) {
+            result.push(left[poinerLeft]);
+            poinerLeft++;
+        } else {
+            result.push(right[pointerRight]);
+            pointerRight++;
+        }
+
+    }
+
+
+    return result.concat(left.slice(poinerLeft)).concat(right.slice(pointerRight));
+}
+
+console.log(mergeSort(randomArr));
 
 
 
